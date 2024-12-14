aac28f74affb80315a67dc3470fa5ddd8236292d
Nimnadee <snr.nimnadi@gmail.com>
2024-12-14 17:49:39 +0530

    undated team.php

--- team.php
+++ team.php
@@ -1,1 +1,187 @@
-php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team - ICET 2025</title>
+    <link rel="icon" type="image/x-icon" href="assets/images/Logo.png">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="./vendor/css/styles.css?v=1.0">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Font-Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- Google-Font -->
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Anton+SC&family=Archivo+Narrow:ital,wght@0,400..700;1,400..700&family=Noto+Sans+Display:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Anton+SC&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Anton+SC&family=Archivo+Narrow:ital,wght@0,400..700;1,400..700&family=Noto+Sans+Display:ital,wght@0,100..900;1,100..900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Anton+SC&family=Archivo+Narrow:ital,wght@0,400..700;1,400..700&family=Noto+Sans+Display:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap');
+    </style>
+
+</head>
+<body>
+<?php include './common/navbar.php'; ?>
+
+<section>
+    <!--        <div style="display: grid; grid-template-columns: repeat(4, 2fr); gap: 0px;" class='dinuja'>-->
+    <!--            <div class="card" style="width: 18rem;">-->
+    <!--                <img src="assets/images/Abeysinghe Shakya photo.jpg" class="card-img-top" alt="...">-->
+    <!--                <div class="card-body">-->
+    <!--                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+    <!--                </div>-->
+    <!--            </div>-->
+    <!---->
+    <!--            <div class="card" style="width: 18rem;">-->
+    <!--                <img src="assets/images/Abeysinghe Shakya photo.jpg" class="card-img-top" alt="...">-->
+    <!--                <div class="card-body">-->
+    <!--                    <p class="card-text">Card 2 content.</p>-->
+    <!--                </div>-->
+    <!--            </div>-->
+    <!---->
+    <!--            <div class="card" style="width: 18rem;">-->
+    <!--                <img src="assets/images/Abeysinghe Shakya photo.jpg" class="card-img-top" alt="...">-->
+    <!--                <div class="card-body">-->
+    <!--                    <p class="card-text">Card 3 content.</p>-->
+    <!--                </div>-->
+    <!--            </div>-->
+    <!---->
+    <!--            <div class="card" style="width: 18rem;">-->
+    <!--                <img src="assets/images/Abeysinghe Shakya photo.jpg" class="card-img-top" alt="...">-->
+    <!--                <div class="card-body">-->
+    <!--                    <p class="card-text">Card 4 content.</p>-->
+    <!--                </div>-->
+    <!--            </div>-->
+    <!---->
+    <!--            <div class="card" style="width: 18rem;">-->
+    <!--                <img src="assets/images/Abeysinghe Shakya photo.jpg" class="card-img-top" alt="...">-->
+    <!--                <div class="card-body">-->
+    <!--                    <p class="card-text">Card 5 content.</p>-->
+    <!--                </div>-->
+    <!--            </div>-->
+    <!---->
+    <!--            <div class="card" style="width: 18rem;">-->
+    <!--                <img src="assets/images/Abeysinghe Shakya photo.jpg" class="card-img-top" alt="...">-->
+    <!--                <div class="card-body">-->
+    <!--                    <p class="card-text">Card 6 content.</p>-->
+    <!--                </div>-->
+    <!--            </div>-->
+    <!---->
+    <!--            <div class="card" style="width: 18rem;">-->
+    <!--                <img src="assets/images/Abeysinghe Shakya photo.jpg" class="card-img-top" alt="...">-->
+    <!--                <div class="card-body">-->
+    <!--                    <p class="card-text">Card 7 content.</p>-->
+    <!--                </div>-->
+    <!--            </div>-->
+    <!---->
+    <!--            <div class="card" style="width: 18rem;">-->
+    <!--                <img src="assets/images/Abeysinghe Shakya photo.jpg" class="card-img-top" alt="...">-->
+    <!--                <div class="card-body">-->
+    <!--                    <p class="card-text">Card 8 content.</p>-->
+    <!--                </div>-->
+    <!--            </div>-->
+    <!--        </div>-->
+
+    <div class="wrapper ">
+        <div class="team">
+            <h1 id='title'>Team Members</h1>
+            <div class="sectors">
+                <div class="sector">
+                    <div class="imgbox">
+                        <img src=" " alt="profile" />
+                    </div>
+                    <div class="intro-text">
+                        <h1> Dr. MDKM Gunasena </h1>
+                    </div>
+                    <span>Conference Chair</span>
+                    <div class="social-links">
+                        <a href="https://www.sab.ac.lk/staff-directory/466">
+                            <button>
+                                <img src="./assets/images/link.png" alt="">
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <div class="sector">
+                    <div class="imgbox">
+                        <img src=" " alt="profile" />
+                    </div>
+                    <div class="intro-text">
+                        <h1>Mr. AMPC Amarasinghe </h1>
+                    </div>
+                    <span>Conference Co-chair</span>
+                    <div class="social-links">
+                        <a href="https://www.sab.ac.lk/staff-directory/474">
+                            <button>
+                                <img src="./assets/images/link.png" alt="">
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <div class="sector">
+                    <div class="imgbox">
+                        <img src=" " alt="profile" />
+                    </div>
+                    <div class="intro-text">
+                        <h1>Mr. ERJMDDP Wijesekara </h1>
+
+                    </div>
+                    <span>Conference Secretory</span>
+                    <div class="social-links">
+                        <a href="https://www.sab.ac.lk/staff-directory/339">
+                            <button>
+                                <img src="./assets/images/link.png" alt="">
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <div class="sector">
+                    <div class="imgbox">
+                        <img src="./assets/images/Abeysinghe Shakya photo.jpg" alt="profile" />
+                    </div>
+                    <div class="intro-text">
+                        <h1>Dr. SS Abeysinghe </h1>
+                    </div>
+                    <span>Conference Co-secretory</span>
+                    <div class="social-links">
+                        <a href=" ">
+                            <button>
+                                <img src="./assets/images/link.png" alt="">
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <div class="sector">
+                    <div class="imgbox">
+                        <img src="./assets/images/Dr.(Ms).H.G.N Rajapaksha.png" alt="profile" />
+                    </div>
+                    <div class="intro-text">
+                        <h1>Dr. HGN Rajapaksha </h1>
+                    </div>
+                    <span>Conference Treasurer</span>
+                    <div class="social-links">
+                        <a href=" ">
+                            <button>
+                                <img src="./assets/images/link.png" alt="">
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="blob"></div>
+
+        </div>
+</section>
+
+<?php include './common/footer1.php'; ?>
+
+<!-- Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
