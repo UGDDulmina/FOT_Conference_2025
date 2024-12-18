b7d9b0f4e8b5b3c74c05e5d68ee9110a01c4afd3
Nimnadee <snr.nimnadi@gmail.com>
2024-12-18 09:20:01 +0530

    updated user cards in team.php (Conference coordinators -section-2)

--- team.php
+++ team.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team - ICET 2025</title>
     <link rel="icon" type="image/x-icon" href="assets/images/Logo.png">
-
+<!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />-->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
 
     <!-- Bootstrap -->
@@ -13,7 +14,9 @@
     <!-- Custom CSS -->
     <link rel="stylesheet" href="./vendor/css/styles.css">
     <!-- Font-Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<!--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+
 
     <!-- Google-Font -->
     <style>
@@ -32,7 +35,7 @@
 
     <div class="slide-container">
         <div class="slide-content">
-            <div class="team-name">KEY MEMBERS</div>
+            <div class="team-name">Key Members</div>
             <div class="card-wrapper">
                 <div class="card">
                     <div class="image-content">
@@ -107,22 +110,122 @@
                         <button class="view-profile-btn"> View Profile</button>
                     </div>
                 </div>
-
-
-
             </div>
+        </div>
+    </div>
 
+</section>
+
+<section class="team-2-section">
+    <div class="curve">
+
+    </div>
+
+</section>
+<section class="team-3-section">
+        <div class="custom-shape-divider-bottom-1734241371">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+            </svg>
         </div>
 
+    <div class="swiper">
+        <div class="slide-content-2">
+            <div class="team-name">Conference Coordinators</div>
+            <div class="swiper-wrapper">
+                <div class="card swiper-slide">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                        <div class="card-image">
+                            <img src="assets/images/Abeysinghe%20Shakya%20photo.jpg" alt="" class="card-img"">
 
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <div class="name"> Dr. MDKM Gunasena</div>
+
+                        <p class="role">Conference Chair</p>
+                        <button class="view-profile-btn"> View Profile</button>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                        <div class="card-image">
+                            <img src="assets/images/A.M.P.C.Amarasinghe.png" alt="" class="card-img"">
+
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <div class="name"> Mr. AMPC Amarasinghe</div>
+
+                        <p class="role">Co-chair</p>
+                        <button class="view-profile-btn"> View Profile</button>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                        <div class="card-image">
+                            <img src="assets/images/ERJMDDP Wijesekara.png" alt="" class="card-img"">
+
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <div class="name">  Mr. ERJMDDP Wijesekara</div>
+
+                        <p class="role">Secretory</p>
+                        <button class="view-profile-btn"> View Profile</button>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                        <div class="card-image">
+                            <img src="assets/images/Abeysinghe%20Shakya%20photo.jpg" alt="" class="card-img"">
+
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <div class="name"> Dr. SS Abeysinghe</div>
+                        <p class="role">Co-secretory</p>
+                        <button class="view-profile-btn"> View Profile</button>
+                    </div>
+                </div>
+                <div class="card swiper-slide">
+                    <div class="image-content">
+                        <span class="overlay"></span>
+                        <div class="card-image">
+                            <img src="assets/images/Dr.(Ms).H.G.N Rajapaksha.png" alt="" class="card-img"">
+
+                        </div>
+                    </div>
+                    <div class="card-content">
+                        <div class="name"> Dr. HGN Rajapaksha</div>
+                        <p class="role">Treasurer</p>
+                        <button class="view-profile-btn"> View Profile</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
     </div>
 
 </section>
 
 <?php include './common/footer1.php'; ?>
 
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>.
+
 <!-- Bootstrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>-->-->
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="vendor/js/script.js"></script>
 
 </body>
+<!--Swiper JS-->
+
 </html>
---
+++ vendor/js/script.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.slide-content-2', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+
+    // Pagination bullets
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+
+});
+
+console.log("script is running");
